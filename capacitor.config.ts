3f83a220d027603675d415fd4c81d8417c59932b
anzar2 <afk656902@hotmail.com>
2023-09-04 13:03:43 -0300

    Creacion del repositorio

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'camellos_food',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
