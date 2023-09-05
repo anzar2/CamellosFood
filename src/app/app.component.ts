184c366a0402941a2f0080eaa936250d982b85d5
AloRzs <alonso.rozas.diaz@gmail.com>
2023-09-05 01:48:54 -0300

    Se crea barra lateral de carrito
    Además de crear la barra del carrito, queda por implementar los botones para desplegarlas en el resto de páginas 🤡🥸

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MenuController } from '@ionic/angular';
+// se crean métodos para abrir las barras laterales de la página -Rozas
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -12,5 +14,17 @@ export class AppComponent {
     { title: 'Carrito', url: '/folder/archived', icon: 'cart' },
     { title: 'Cerrar sesión', url: '/folder/trash', icon: 'power' },
   ];
-  constructor() {}
+  constructor(private menuCtrl: MenuController) {}
+
+  openFirstMenu() {
+    // Open the menu by menu-id
+    this.menuCtrl.enable(true, 'menu-principal');
+    this.menuCtrl.open('menu-principal');
+  }
+  openEndMenu() {
+    // Open the menu by side
+    this.menuCtrl.enable(true, 'carrito');
+    this.menuCtrl.open('carrito');
+  }
 }
+
