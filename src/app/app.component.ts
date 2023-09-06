2bef1205f1723ac519b228fbd0dda945ab8c4ae1
Benjamín Cuevas <benjamin.cuevas1312@gmail.com>
2023-09-06 11:55:28 -0300

    Se mejoro considerablemente el toolbar del inicio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { MenuController } from '@ionic/angular';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss'],
+  styleUrls: ['app.component.scss', '../camellosfood.theme.scss'],
 })
 export class AppComponent {
   public appPages = [
